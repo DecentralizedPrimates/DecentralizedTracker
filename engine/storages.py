7d67ecc61df64391b31981c90509ce4089a83189
Svyatozar <andreysvatozar@gmail.com>
2021-11-25 09:30:10 +0200

    Tags table structure & datetime problem fixed

--- engine/storages.py
+++ engine/storages.py
@@ -1,8 +1,18 @@
 from abc import ABC, abstractmethod
 from messages import TagMessage, OpinionMessage
-
+from dbConnector import DBConnector
 
 class TagStorage(ABC):
+
+    def __init__(self):
+        DBConnector().execute("""
+            CREATE TABLE IF NOT EXISTS Tags (
+                info_hash TEXT PRIMARY KEY,
+                attribute TEXT,
+                time TIMESTAMP,
+                salt TEXT
+            );
+        """)
 
     @abstractmethod
     def contains_tag(self, tag: TagMessage) -> bool:
@@ -19,3 +29,7 @@ class OpinionStorage(ABC):
     def increment_opinion(self, tag: TagMessage):
         pass
 
+
+if __name__ == '__main__':
+    db_connection = sqlite3.connect('storage/db.sqlite3')
+    cursor = db_connection.cursor()
