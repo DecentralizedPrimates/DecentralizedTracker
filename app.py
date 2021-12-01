f1f7551e9fc07f5678480b3e80a0bcdf602fbc89
Ivan Myasoyedov <ivan.myasoyedov@gmail.com>
2021-12-01 14:42:58 +0200

    Rewrite API template with FastAPI
    Make simple FastAPI app with blank endpoints
    from given documentation.

--- app.py
+++ app.py
@@ -1,33 +1,77 @@
-from flask import Flask, request
+import uvicorn as uvicorn
+from fastapi import FastAPI
 
-from mocks.download_torrent_handler_mock import DownloadTorrentHandlerMock
+# For interacting with the network local client API should contain the following endpoints:
+#
+# MainDefaultSreen to display files by default on main screen. (GET)
+# FileSearch to find a file by name and other parameters (GET)
+# Settings (POST)
+# Upload (POST)
+# AddCategorie (POST)
+# Download (GET)
+
+app = FastAPI(
+    title="DecetralizedTracker API",
+    description="Local client API for interacting with the network.",
+    version="0.1.0",
+    openapi_tags=[
+        {
+            "name": "MainDefaultSreen",
+            "description": "Display files by default on main screen.",
+        },
+        {
+            "name": "FileSearch",
+            "description": "Find a file by name and other parameters.",
+        },
+        {
+            "name": "Settings",
+            "description": "",
+        },
+        {
+            "name": "Upload",
+            "description": "",
+        },
+        {
+            "name": "AddCategorie",
+            "description": "",
+        },
+        {
+            "name": "Download",
+            "description": "",
+        },
+    ],
+)
 
 
-app = Flask(__name__)
+@app.get("/", tags=["MainDefaultSreen"])
+async def main_default_screen():
+    return {""}
 
-download_torrent_handler = DownloadTorrentHandlerMock()
+
+@app.get("/file_search", tags=["FileSearch"])
+async def file_search():
+    return {""}
 
 
-@app.route('/download_torrent', methods=['GET', 'POST'])
-def login():
-    if request.method == 'GET':
-        try:
-            # using query params
-            response = download_torrent_handler.get_params(**request.args)
-        except Exception:
-            return None
+@app.get("/settings", tags=["Settings"])
+async def settings():
+    return {""}
 
-        return response
 
-    if request.method == 'POST':
-        try:
-            # using request body data
-            response = download_torrent_handler.get_params(**request.json)
-        except Exception:
-            return None
+@app.post("/upload", tags=["Upload"])
+async def upload():
+    return {""}
 
-        return response
+
+@app.post("/add_categorie", tags=["AddCategorie"])
+async def add_categorie():
+    return {""}
+
+
+@app.get("/download", tags=["Download"])
+async def download():
+    return {""}
 
 
 if __name__ == "__main__":
-    app.run(host="127.0.0.1", port=5300, debug=True)
+    uvicorn.run("app:app", host="0.0.0.0", port=8000, reload="True", log_level="info")
