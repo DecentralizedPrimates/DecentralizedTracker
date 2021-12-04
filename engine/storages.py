5d99256ce2350aa9fb1b4f6aa28f40df4bd8eaef
kyrylo <cyrillicw@gmail.com>
2021-12-04 22:25:56 +0200

    Revert "Tags table structure & datetime problem fixed"
    This reverts commit 7d67ecc61df64391b31981c90509ce4089a83189.

--- engine/storages.py
+++ engine/storages.py
@@ -1,18 +1,8 @@
 from abc import ABC, abstractmethod
 from messages import TagMessage, OpinionMessage
-from dbConnector import DBConnector
+
 
 class TagStorage(ABC):
-
-    def __init__(self):
-        DBConnector().execute("""
-            CREATE TABLE IF NOT EXISTS Tags (
-                info_hash TEXT PRIMARY KEY,
-                attribute TEXT,
-                time TIMESTAMP,
-                salt TEXT
-            );
-        """)
 
     @abstractmethod
     def contains_tag(self, tag: TagMessage) -> bool:
@@ -29,7 +19,3 @@ class OpinionStorage(ABC):
     def increment_opinion(self, tag: TagMessage):
         pass
 
-
-if __name__ == '__main__':
-    db_connection = sqlite3.connect('storage/db.sqlite3')
-    cursor = db_connection.cursor()
