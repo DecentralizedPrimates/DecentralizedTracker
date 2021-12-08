3b9ed487ac1c17aba8dd7a2dbd119f323c58a357
kyrylo <cyrillicw@gmail.com>
2021-12-08 21:48:06 +0200

    Download Path

--- engine/configs.py
+++ engine/configs.py
@@ -8,7 +8,7 @@ class AppConfig:
         configs = configparser.ConfigParser()
         configs.read(config_file)
         self.ip = configs['DEFAULT']['IP']
-        self.flask_port = int(configs['DEFAULT']['FlaskPort'])
+        self.flask_port = int(configs['DEFAULT']['ServerPort'])
         self.dht_port = configs['DEFAULT']['DhtPort']
         bootstrap_nodes = json.loads(configs['DEFAULT']['Bootstrap'])
         self.bootstrap_nodes = []
@@ -16,3 +16,4 @@ class AppConfig:
             ip, port = full_address.split(":")
             port = int(port)
             self.bootstrap_nodes.append((ip, port))
+        self.download_path = configs['DEFAULT']['DownloadPath']
